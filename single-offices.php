7d29224532fc28f6b0be3169b16d9879cd89a5e4
Nic Winn <9751044+nicwinn@users.noreply.github.com>
2017-08-24 23:31:29 -0500

    code formatting

--- single-offices.php
+++ single-offices.php
@@ -12,15 +12,14 @@ get_header(); ?>
 
 <?php
 //if 'show hero' image has been selected
-if ( get_field('uthsc_show_post_hero_image', $post_id) ) {
+if (get_field('uthsc_show_post_hero_image', $post_id)) {
     //if a hero image has been uploaded
     $hero_img = get_field('uthsc_post_hero_image');
-    if ( !empty( $hero_img ) ){
+    if (!empty($hero_img)) {
 
         //load the post hero image
         get_template_part('template-parts/uthsc-post-hero');
-    }
-    else {
+    } else {
         //otherwise load the featured image
         get_template_part('template-parts/featured-image');
     }
@@ -28,19 +27,18 @@ if ( get_field('uthsc_show_post_hero_image', $post_id) ) {
 ?>
     <div id="single-post" role="main" class="row">
 
-        <?php do_action( 'foundationpress_before_content' ); ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-            <div class="columns">
-                <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-                    <header>
-                        <h1 class="entry-title"><?php the_title(); ?></h1>
-                    </header>
-                    <?php do_action( 'foundationpress_post_before_entry_content' ); ?>
-                    <div class="entry-content">
-                        <div class="row">
-                            <div class="columns medium-6 large-8">
-                                <figure class="uthsc-figure thumbnail care-streetview-iframe">
-                                    <?php echo uthsc_get_acf('care_iframe', get_the_ID(), '', ''); ?>
+        <?php do_action('foundationpress_before_content'); ?>
+        <?php while (have_posts()) :
+        the_post(); ?>
+        <div class="columns">
+            <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+                <header>
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </header>
+                <?php do_action('foundationpress_post_before_entry_content'); ?>
+                <div class="entry-content">
+                    <div class="row">
+                        <div class="columns medium-6 large-8">
                             <!--============================================-->
                             <?php
                             $care_image = uthsc_get_acf('care_image_url', get_the_ID(), '', '');
@@ -114,25 +112,32 @@ if ( get_field('uthsc_show_post_hero_image', $post_id) ) {
                                 </dt>
                             </dl>
 
-                        <!--                        --><?php //echo do_shortcode('[offices]'); ?>
+
+                            <a class="button large fa fa-comment expanded"> Contact this office</a>
 
 
-
-                        <?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+                        </div>
                     </div>
-                    <footer>
-                        <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-                        <p><?php the_tags(); ?></p>
-                    </footer>
-                    <!--      --><?php //the_post_navigation(); ?>
-                    <?php do_action( 'foundationpress_post_before_comments' ); ?>
-                    <?php comments_template(); ?>
-                    <?php do_action( 'foundationpress_post_after_comments' ); ?>
-                </article>
-            </div>
-        <?php endwhile;?>
+                </div>
+                <!--                        --><?php //echo do_shortcode('[offices]'); ?>
 
-        <?php do_action( 'foundationpress_after_content' ); ?>
+
+
+                <?php edit_post_link(__('Edit', 'foundationpress'), '<span class="edit-link">', '</span>'); ?>
+        </div>
+        <footer>
+            <?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'foundationpress'), 'after' => '</p></nav>')); ?>
+            <p><?php the_tags(); ?></p>
+        </footer>
+        <!--      --><?php //the_post_navigation(); ?>
+        <?php do_action('foundationpress_post_before_comments'); ?>
+        <?php comments_template(); ?>
+        <?php do_action('foundationpress_post_after_comments'); ?>
+        </article>
+    </div>
+<?php endwhile; ?>
+
+<?php do_action('foundationpress_after_content'); ?>
 
     </div>
 <?php get_footer();
