cdf1e3d53d8f7f9ed4acda92c9230c9fda48e33d
Nic Winn <9751044+nicwinn@users.noreply.github.com>
2017-08-24 23:33:25 -0500

    trimmed down page to only show list of buttons

--- template-parts/content-offices.php
+++ template-parts/content-offices.php
@@ -7,26 +7,10 @@
  * @package FoundationPress
  * @since FoundationPress 1.0.0
  */
-
 ?>
-
-<!--<a style="border: 1px solid;" href="--><?php //the_permalink(); ?><!--" class="column column-block " id="post---><?php //the_ID(); ?><!--" --><?php //post_class('blogpost-entry'); ?>
-<!--    <header>-->
-<!--        <h2>--><?php //the_title(); ?><!--</h2>-->
-<!--    </header>-->
-<!--    <div class="entry-content">-->
-<!--        --><?php //the_content( __( 'Continue reading...', 'foundationpress' ) ); ?>
-<!--    </div>-->
-<!--    <footer>-->
-<!--        --><?php //$tag = get_the_tags(); if ( $tag ) { ?><!--<p>--><?php //the_tags(); ?><!--</p>--><?php //} ?>
-<!--        --><?php //edit_post_link('Edit Post','',''); ?>
-<!--    </footer>-->
-<!--</a>-->
-
-<a href="<?php the_permalink(); ?>" class="button large column column-block " id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
-
-
-    <?php the_title(); ?>
-
-
-</a>
+<div class="care-office-button column column-block">
+    <a href="<?php the_permalink(); ?>" class="button expanded"
+       id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?> data-equalizer-watch>
+        <?php the_title(); ?>
+    </a>
+</div>
